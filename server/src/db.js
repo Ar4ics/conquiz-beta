d94583c9ad30f89bd90de902d5bcb0a9701f34bc
Ar4ics <antivirus4comp@gmail.com>
2017-12-22 15:32:38 +0500

    heroku deploy

--- server/src/db.js
+++ server/src/db.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb://127.0.0.1/conquiz';
+const mongoDB = MONGOLAB_URI || 'mongodb://127.0.0.1/conquiz';
 mongoose.connect(mongoDB, {
-    useMongoClient: true
+  useMongoClient: true
 });
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 
 db.once('open', () => {
-    console.log('conn opened');
+  console.log('conn opened');
 })
 
 db.on('error', function (error) {
-    console.error('error in MongoDb connection: ' + error);
-    mongoose.disconnect();
+  console.error('error in MongoDb connection: ' + error);
+  mongoose.disconnect();
 });
 
 db.on('close', function () {
-    console.log('conn closed');
+  console.log('conn closed');
 });
 
 module.exports = db;
