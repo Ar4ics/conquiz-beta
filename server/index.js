91af3f423eceec3a6855a88c7bc675d650a95554
Ar4ics <antivirus4comp@gmail.com>
2017-12-22 15:46:16 +0500

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const bodyParser = require('body-parser');
 const db = require('./src/db');
+const path = require('path');
 const websocket = require('./src/websocket');
 const shuffleArray = require('./src/helpers/shuffle');
 const app = express();
@@ -10,11 +11,9 @@ const port = 3000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 websocket(wss, WebSocket.OPEN);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
